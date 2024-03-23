3922e77ded193371286653e2342432182fbdbcd2
vivekverma4669 <vivekverma4679@gmail.com>
2024-03-23 17:20:48 +0530

    front-end 95% done

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,21 @@
 import './App.css';
-import TaskListList from './Components/TaskList';
-import AddTask from './Components/AddTask';
 import Nav from './Components/Nav';
 import Footer from './Components/Footer';
 import { BrowserRouter } from 'react-router-dom';
-import Home from './Components/Home';
 import AllRoutes from './AllRoutes/AllRoutes';
 import { AuthProvider } from './Authcontext';
 
 
 function App() {
   return (
-    <div>
+    <div className="App">
     <BrowserRouter>
     <AuthProvider>
-    <div className="App">
+  
     <Nav/>
     <AllRoutes/>
-
     <Footer/>
-    </div>
+
     </AuthProvider>
     </BrowserRouter>
 
